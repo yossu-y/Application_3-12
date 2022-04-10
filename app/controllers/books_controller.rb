df71b591af5ab7641872139bd7dc79167d1564b5
yossu-y <yangyuub@gmail.com>
2022-04-10 12:52:19 +0000

    [finish]checksheet13

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,7 +25,7 @@ class BooksController < ApplicationController
   end
 
   def update
-    @book = Book.find(params[:id])
+    book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book.id)
   end
