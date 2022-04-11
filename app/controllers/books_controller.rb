d4a7541f99ad207bfd699a12bc6e552e1f042351
yossu-y <yangyuub@gmail.com>
2022-04-11 06:21:02 +0000

    [update]レイアウト

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,9 +3,10 @@ class BooksController < ApplicationController
     @book = Book.new
     @books = Book.all
   end
-  
+
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
@@ -14,6 +15,7 @@ class BooksController < ApplicationController
       redirect_to book_path(@book.id)
       flash[:notice] = "Book was successfully created."
     else
+      @books = Book.all
       render :new
     end
   end
@@ -27,9 +29,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+      flash[:notice] = "Book was successfully updated."
+    else
+      render :edit
+    end
   end
 
   def destroy
