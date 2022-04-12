d02c389b5ee291d9421458f060cebb7109e89b9e
yossu-y <yangyuub@gmail.com>
2022-04-12 14:04:24 +0000

    [finish]gemfile-bundle install

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_10_074649) do
-
-  create_table "books", force: :cascade do |t|
-    t.string "title"
-    t.string "body"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
